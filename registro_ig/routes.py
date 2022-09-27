41d73445e87cc4896f3ca7eb4029fb208680a5c4
Zyonkas <zhujiankai25@gmail.com>
2022-09-27 18:17:38 +0200

    Fin first

--- registro_ig/routes.py
+++ registro_ig/routes.py
@@ -1,22 +1,15 @@
-from crypt import methods
-from flask import render_template, request, redirect
+import re
+from flask import render_template, request, redirect, url_for
 import csv
 from registro_ig import app
 from datetime import date
-
+from config import *
+from registro_ig.models import delete_by, select_all, select_by, insert, update_by
+import os
 
 @app.route("/")
 def index():
-    fichero = open("data/movimientos.txt", "r")
-    csvReader = csv.reader(fichero, delimiter=',', quotechar='"')
-    movimientos = []
-    for movimiento in csvReader:
-        movimientos.append(movimiento)
-
-    # movimientos = [movimiento for movimiento in csvReader] #list comprehension
-
-    fichero.close()
-    return render_template("index.html", pageTitle="Lista", movements=movimientos)
+    return render_template("index.html", pageTitle="Lista", movements=select_all())
 
 @app.route("/nuevo", methods=["GET", "POST"])
 def alta():
@@ -33,15 +26,12 @@ def alta():
         errores = validaFormulario(request.form)
 
         if not errores:
-            fichero = open("data/movimientos.txt", "a", newline="")
-            csvWriter = csv.writer(fichero, delimiter=",", quotechar='"')
-
-            # Generar un nuevo id
-
-            csvWriter.writerow([request.form['date'], request.form['concept'], request.form['quantity']])
-            fichero.close()
-
-            return redirect("/")
+            #Obtener el nuevo id
+            insert([request.form['date'],
+                    request.form['concept'],
+                    request.form['quantity']
+                  ])
+            return redirect(url_for("index"))
         else:
             return render_template("new.html", pageTitle="Alta", msgErrors=errores, dataForm=dict(request.form))
 
@@ -49,7 +39,7 @@ def validaFormulario(camposFormulario):
     errores = []
     hoy = date.today().isoformat()
     if camposFormulario['date'] > hoy:
-        errores.append("La fecha introducida es es futuro.")
+        errores.append("La fecha introducida es el futuro.")
 
     if camposFormulario['concept'] == "":
         errores.append("Introduce un concepto para la transacción.")
@@ -61,6 +51,12 @@ def validaFormulario(camposFormulario):
 
     return errores
 
+def form_to_list(id, form):
+    return [str(id),
+            form['date'], 
+            form['concept'],
+            form['quantity']
+            ]
     
 @app.route("/modificar/<int:id>", methods=["GET", "POST"])
 def modifica(id):
@@ -69,8 +65,8 @@ def modifica(id):
         1. Consultar en movimientos.txt y recueperar el registro con id al de la petición
         2. Devolver el formulario html con los datos de mi registro
         """
-
-        return render_template("modifica.html", registro=[])
+        register = select_by(id)
+        return render_template("update.html", registro=register, pageTitle = "Modificar")
     else:
         """
         1. validar registro de entrada
@@ -78,7 +74,15 @@ def modifica(id):
         3. redirect 
         4. Si el registro es incorrecto la gestion de errores que conocemos
         """
-        pass
+        errores = validaFormulario(request.form)
+
+        if not errores:
+            update_by(form_to_list(id, request.form))
+            return redirect(url_for("index"))
+        else:
+            return render_template("update.html", registro=form_to_list(id, request.form), pageTitle = "Modificar", msgErrors=errores)
+
+
 
 @app.route("/borrar/<int:id>", methods=["GET", "POST"])
 def borrar(id):
@@ -88,10 +92,22 @@ def borrar(id):
         2. Devolver el formulario html con los datos de mi registro, no modificables 
         3. Tendrá un boton que diga confirmar.
         """
-        return render_template("borra.html", registro=[])
+        registro_definitivo = select_by(id)
+        if registro_definitivo:
+            return render_template("delete.html", registro=registro_definitivo)
+        else:
+            return redirect(url_for("index"))
     else:
         """
             Borrar el registro
+            1. abrir fichero movimientos.txt en lectura
+            2. abrir fichero nmovimientos.txt en escritura
+            3. copiar todo los registros uno a uno en su orden exceptuando el que queremos borrar
+            4. borrar movimiento.txt
+            5. renombrar nmovimeintos.txt a movmientos.txt
         """       
-        pass
-         
+        delete_by(id)
+
+        return redirect(url_for("index"))
+
+ 
